356d582cda6f6ba64e114bda82dccc949ed9580a
donkrasnov <krasnov@barcamania.com>
2023-09-17 01:49:07 +0300

    update-wait-by-methods

--- waitByMethods.py
+++ waitByMethods.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.common import NoSuchElementException, TimeoutException
+from selenium.common import NoSuchElementException, TimeoutException, ElementClickInterceptedException
 
-WAIT_TIMEOUT = 10
+WAIT_TIMEOUT = 30
 
 
 def wait_visibility_by_name(driver: webdriver, name: str):
@@ -39,6 +39,16 @@ def wait_invisibility_by_xpath(driver: webdriver, xpath: str):
         print(f'[ERROR] {current_timestamp} ---> XPath "{xpath}" is visible!')
 
 
+def wait_invisibility_by_class_name(driver: webdriver, class_name: str):
+    try:
+        WebDriverWait(driver, WAIT_TIMEOUT).until(
+            ec.invisibility_of_element_located((By.CLASS_NAME, class_name))
+        )
+    except (TimeoutException, NoSuchElementException):
+        current_timestamp = datetime.now()
+        print(f'[ERROR] {current_timestamp} ---> XPath "{class_name}" is visible!')
+
+
 def wait_visibility_by_id(driver: webdriver, target_id: str):
     try:
         WebDriverWait(driver, WAIT_TIMEOUT).until(
@@ -57,3 +67,23 @@ def wait_vilibility_by_class_name(driver: webdriver, class_name: str):
     except (TimeoutException, NoSuchElementException):
         current_timestamp = datetime.now()
         print(f'[ERROR] {current_timestamp} ---> Class name "{class_name}" nis not visible!')
+
+
+def wait_clickable_by_id(driver: webdriver, id_value: str):
+    try:
+        WebDriverWait(driver, WAIT_TIMEOUT).until(
+            ec.element_to_be_clickable((By.ID, id_value))
+        )
+    except ElementClickInterceptedException:
+        current_time = datetime.now()
+        print(f"[ERROR] {current_time} ---> Element with ID {id_value} not clickable!")
+
+
+def wait_clickable_by_xpath(driver: webdriver, path: str):
+    try:
+        WebDriverWait(driver, WAIT_TIMEOUT).until(
+            ec.element_to_be_clickable((By.XPATH, path))
+        )
+    except ElementClickInterceptedException:
+        current_time = datetime.now()
+        print(f"[ERROR] {current_time} ---> Element with XPath {path} not clickable!")
