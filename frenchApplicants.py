c859886c89feb6c119a86987eb2e841c91a36be7
donkrasnov <krasnov@barcamania.com>
2023-09-20 20:24:05 +0300

    important! add new test user 2

--- frenchApplicants.py
+++ frenchApplicants.py
@@ -5,42 +5,46 @@ from dataclasses import dataclass
 class Applicant:
     code: str
     phone: str
+    login_email: str
     email: str
-    login: str
-    password_email: str
     password_vfs: str
-    travel_date: str
     first_name: str
     last_name: str
     birth_date: str
     passport_no: str
-    issue_date: str
     exp_date: str
     issue_place: str
     gender: str
-    travel_start: str
-    travel_end: str
 
 
 test_user = Applicant(
     '7',
     '9252090883',
-    # 'VANELI@YANDEX.RU',
-    'INTCHERNOV@YANDEX.RU',
-    'Vaneli',
-    '240297-',
+    # 'INTCHERNOV@YANDEX.RU'
+    'VANELI@YANDEX.RU',
+    'VANELI@YANDEX.RU',
     '240297Vis@',
-    '2023-10-27',
     'IVAN',
     'KRASNOV',
-    # '03/30/1996',
     '30031996',
     '732842880',
-    '2014-05-16',
-    # '05/16/2024',
     '16052024',
     'FMS34001',
     'Male',
-    '01112023',
-    '10112023'
+)
+
+
+test_user_2 = Applicant(
+    '7',
+    '9252101020',
+    'VANELI@YANDEX.RU',
+    'VAN321@BK.RU',
+    '240297Vis@',
+    'IVAN',
+    'KRASNOV',
+    '23031996',
+    '732882881',
+    '18072026',
+    'FMS34003',
+    'Male',
 )
